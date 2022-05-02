64eb9e134976c161e01edff9a126f5f056da0cbd
MSLEE <leems041625@gmail.com>
2022-05-02 16:20:42 +0900

    [feat] Nearby, price_compare view and Fragment

--- app/src/main/java/com/example/gajang/view/NearbyStoreFragment.kt
+++ app/src/main/java/com/example/gajang/view/NearbyStoreFragment.kt
@@ -1,15 +1,38 @@
 package com.example.gajang.view
 
+import android.view.View
+import android.widget.AdapterView
 import com.example.gajang.R
 import com.example.gajang.base.BaseFragment
 import com.example.gajang.databinding.FragmentNearbyStoreBinding
+import android.widget.ArrayAdapter as ArrayAdapter
 
 class NearbyStoreFragment : BaseFragment<FragmentNearbyStoreBinding>(R.layout.fragment_nearby_store) {
-    override fun FragmentNearbyStoreBinding.onCreateView(){
+    override fun FragmentNearbyStoreBinding.onCreateView() {
 
     }
 
-    override fun FragmentNearbyStoreBinding.onViewCreated(){
+    override fun FragmentNearbyStoreBinding.onViewCreated() {
+        var marketData = resources.getStringArray(R.array.living_array)
+        var marketImage = resources.getStringArray(R.array.item_image)
+        var adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, marketData)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        nearbyChoiceItemSpinner.adapter = adapter
 
+        nearbyChoiceItemSpinner.setSelection(1)
+
+        nearbyChoiceItemSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
+
 }
+
+
--- app/src/main/java/com/example/gajang/view/PriceCompareFragment.kt
+++ app/src/main/java/com/example/gajang/view/PriceCompareFragment.kt
@@ -1,5 +1,8 @@
 package com.example.gajang.view
 
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.gajang.R
 import com.example.gajang.base.BaseFragment
 import com.example.gajang.databinding.FragmentPriceCompareBinding
@@ -10,6 +13,23 @@ class PriceCompareFragment : BaseFragment<FragmentPriceCompareBinding>(R.layout.
     }
 
     override fun FragmentPriceCompareBinding.onViewCreated(){
+        var itemData = resources.getStringArray(R.array.item_array)
+        var itemImage = resources.getStringArray(R.array.item_image)
+        var adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, itemData)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        priceCompareMarketSpinner.adapter = adapter
 
+        priceCompareMarketSpinner.setSelection(1)
+
+        priceCompareMarketSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 }
